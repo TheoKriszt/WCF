ccae1fa223fdf9560c26a28a2f6c61c86b10e71c
Theo <theo@kriszt.fr>
2018-03-12 12:13:12 +0100

    Version alpha, le service de base, le mex et le WSDL marchent... Parfois.

---
+++ ServiceHostApp/ServiceHostApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary;
+
+namespace ServiceHostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (ServiceHost serviceHost = new ServiceHost(typeof(Service1)))
+            {
+                try
+                {
+                    // Open the  ServiceHost  to  start  listening  for  messages .
+                    serviceHost.Open();
+                    // The  service  can now be  accessed .
+                    Console.WriteLine("The service is ready");
+                    Console.WriteLine("Press ENTER to terminate the service");
+                    Console.ReadLine();
+                    //  Close  the  ServiceHost .
+                    serviceHost.Close();
+                }
+                catch (TimeoutException timeProblem)
+                {
+                    Console.WriteLine(timeProblem.Message);
+                    Console.ReadLine();
+                }
+                catch (CommunicationException commProblem)
+                {
+                    Console.WriteLine(commProblem.Message);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ WcfClient/WcfClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+//using WcfServiceLibrary;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IService1 serviceProxy = new ChannelFactory<IService1>("ServiceConfiguration").CreateChannel();
+            ServiceReference1.IService1 serviceProxy = new ChannelFactory<ServiceReference1.IService1>("BasicHttpBinding_IService1").CreateChannel();
+            Console.WriteLine(serviceProxy.GetData(1));
+
+            ServiceReference1.Service1Client proxy = new ServiceReference1.Service1Client();
+            String s = proxy.GetData(0);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
